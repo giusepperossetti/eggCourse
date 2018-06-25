001f7f354d91261de5d9710e8bf201a08af55455
unknown <peppeross995@gmail.com>
2018-06-25 19:47:26 +0200

    seguihttps://www.youtube.com/watch?v=oZGmHNZv7Sc&index=25&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 var express = require("express");
 var app = express();
 
-// Richiede Jade
+// Richiede EJS
 
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // Middleware per funzioni statiche
 
@@ -23,10 +23,18 @@ var users = [];
 // Pagine
 
 app.get('/', function(req, res) {
-  res.render('index', {
-    title: 'Ciao!', message: 'prova ricarica con Nodemon'
-  })
-})
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/contact', function (req, res) {
+  res.sendFile(__dirname + '/contact.html');
+});
+
+app.get('/profile/:name', function(req, res) {
+  res.render('profile');
+});
+
+// Server Listen
 
 var server = app.listen(3000, function() {
   console.log("Server running at http://localhost:" + server.address().port);
