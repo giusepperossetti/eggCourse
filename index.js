43c7ca3c7df055ba2b4bbdae3dcb9d102bc16b4a
unknown <peppeross995@gmail.com>
2018-06-26 18:55:59 +0200

    Funzioni generali di ExpressJS. Fine corso.

--- index.js
+++ index.js
@@ -3,35 +3,39 @@
 var express = require("express");
 var app = express();
 
-// Richiede EJS
+// Richiede BodyParser
+
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Richiede EJS view engine
 
 app.set('view engine', 'ejs');
 
 // Middleware per funzioni statiche
 
-app.use(express.static('public'));
-
-// Richiede FS
-
-var fs = require("fs");
-
-// Richiede Lodash
-
-var _ = require("lodash");
-var users = [];
+app.use('/assets', express.static('assets'));
 
 // Pagine
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
 app.get('/contact', function (req, res) {
-  res.sendFile(__dirname + '/contact.html');
+  res.render('contact', {qs: req.query});
 });
 
 app.get('/profile/:name', function(req, res) {
-  res.render('profile');
+  var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
+  res.render('profile', {person: req.params.name, data: data});
+});
+
+// Post request per pagina contact
+
+app.post('/contact', urlencodedParser, function (req, res) {
+  console.log(req.body);
+  res.render('contact-success', { data: req.body });
 });
 
 // Server Listen
